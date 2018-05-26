5095c70030bb032a5f44274651ffdf4df7d2e5db
portal <gabriel.o.portal@gmail.com>
2018-05-26 12:08:18 -0300

    finalização das funçoes basicas do EE84

--- quantumEncryption/Pessoa.py
+++ quantumEncryption/Pessoa.py
@@ -35,12 +35,11 @@ class Pessoa():
             return 1
         else:
             v = randint(0,3)
-            if(v == 0): return ""
-            elif(v==1): return ""
+            if(v == 0): return -1
+            elif(v==1): return -1
             elif(v==2): return 1
             else: return 0
             
-
     def geraBasesAleatorias(self,nBases):
         self.bases = range(nBases)
         # 1 retilinea
@@ -64,13 +63,67 @@ class Pessoa():
         return qubits
 
     def lerQubits(self,qubits):
-        print("qbits alice leu",qubits)
         bits = range(len(qubits))
         bases = self.bases
         for I in bits:
             bits[I] = self.lerQubit(qubits[I],bases[I])
         self.bits = bits
         
+    def enviaBaseRevisao(self):
+        bases = self.bases[:]
+        bits = self.bits[:]
+        basesRevis = range(len(self.bits))
+        for I in basesRevis:
+            if (bits[I] != -1):
+                basesRevis[I] = bases[I]
+            else:
+                basesRevis[I] = -1
+        return basesRevis
+
+    def revisaBase(self,bases):
+        myBases = self.bases
+        tBases = range(len(bases))
+        for I in tBases:
+            if bases[I] == myBases[I]:
+                tBases[I] = True
+            else:
+                tBases[I] = False
+        return tBases
+
+    def recebeRevisaBase(self,basesRevisadas):
+        for I in range(len(basesRevisadas)):
+            if(basesRevisadas[I] == False):
+                self.bits[I] = -1
+            
+    def enviaBitRevisao(self):
+        myBits = self.bits
+        sendBits = myBits[:]
+        iHBits = 0
+        for I in range(len(sendBits)):
+            if sendBits[I] != -1:
+                sendBits[I] = -1
+                iHBits += 1
+        I=0
+        while I<iHBits//3:
+            aux = randint(0,len(myBits)-1)
+            if myBits[aux] != -1:
+                sendBits[aux] = myBits[aux]
+                self.bits[aux] = -1
+                I+=1
+        return sendBits
+
+    def revisaBit(self,bits):
+        for I in range(len(self.bits)):
+            if(self.bits[I] != bits[I] and bits[I] != -1):
+                return False
+        return True
+
+    def bitsSecretos(self):
+        sBits = []
+        for I in range(len(self.bits)):
+            if(self.bits[I] != -1):
+                sBits.append(self.bits[I])
+        return sBits
 
     # def lerQubits(self,qubits,bases):
     #     for I in range(bases):
--- quantumEncryption/main.py
+++ quantumEncryption/main.py
@@ -6,16 +6,22 @@ from Canal import Canal
 
 bob = Pessoa("bob")
 alice = Pessoa("alice")
+eve = Pessoa("eve")
 
 nQubits = input("quantos qubits? ")
 
 bob.geraBasesAleatorias(nQubits)
 bob.geraBitsAleatorios(nQubits)
-alice.geraBasesAleatorias(nQubits)
 
+alice.geraBasesAleatorias(nQubits)
 
 canal = Canal(bob.enviarQubits())
 
+onEve = input("Com eve? 1-T 0-F")
+
+if onEve:
+    eve.geraBasesAleatorias(nQubits)
+    eve.lerQubits(canal.getQubits())
 
 alice.lerQubits(canal.getQubits())
 
@@ -25,12 +31,14 @@ print("  bases bob: ",bob.bases)
 print("bases alice: ",alice.bases)
 print("bits alice: ",alice.bits)
 
+print("bases enviadas para revisao: ",bob.revisaBase(alice.enviaBaseRevisao()))
+alice.recebeRevisaBase(bob.revisaBase(alice.enviaBaseRevisao()))
+print("bits alice revisados: ",alice.bits)
 
+print("bits para revisao: ",alice.enviaBitRevisao())
 
-# if(input("bits aleatorios? ") == 1):
-    
-# else:
-    # print(alice.nome)
-
-
-# randint(0,1)
+if bob.revisaBit(alice.enviaBitRevisao()):
+    print("bits compartilhados seguramente")
+    print("bits secretos",alice.bitsSecretos())
+else:
+    print("alguem interceptou os bits!")
